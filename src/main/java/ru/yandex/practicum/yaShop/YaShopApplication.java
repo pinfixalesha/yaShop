f76ac2244922bb495573d0b36312a6c843241521
pav <pav@finstream.ru>
2025-02-27 16:21:27 +0300

    Инициализация проекта

---
+++ src/main/java/ru/yandex/practicum/yaShop/YaShopApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.yaShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/yandex/practicum/yaShop/YaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.yaShop;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class YaShopApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
